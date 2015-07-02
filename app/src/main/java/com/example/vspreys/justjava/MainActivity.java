cbea985810c5064ee9105e2f077fe20609eaf595
VSpreys <vladislav@spreys.com>
2015-07-02 20:37:55 +1200

    something is working

--- app/src/main/java/com/example/vspreys/justjava/MainActivity.java
+++ app/src/main/java/com/example/vspreys/justjava/MainActivity.java
@@ -1,47 +1,68 @@
 package com.example.vspreys.justjava;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.text.NumberFormat;
 
-
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends ActionBarActivity {
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     /**
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
+    /**
+     * This method is called when the plus button is clicked.
+     */
+     public void increment(View view) {
+         display(++quantity);
+
+         findViewById(R.id.minusButton).setEnabled(quantity > 0);
+         findViewById(R.id.plusButton).setEnabled(quantity < 10);
+         findViewById(R.id.orderButton).setEnabled(quantity > 0);
+     }
+    /**
+     * This method is called when the minus button is clicked
+     */
+    public void decrement (View view) {
+        display(--quantity);
+
+        findViewById(R.id.minusButton).setEnabled(quantity > 0);
+        findViewById(R.id.plusButton).setEnabled(quantity <= 10);
+        findViewById(R.id.orderButton).setEnabled(quantity > 0);
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
